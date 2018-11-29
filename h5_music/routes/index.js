a94b74dd628ad903d17b4892791fcdbd28d01593
王一一鹤 <190768613@qq.com>
2018-11-29 11:56:38 +0800

    HTML5实现音乐可视化

---
+++ h5_music/routes/index.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var router = express.Router();
+var mediaPath = 'public/media';
+router.get('/', function(req, res){
+	var fs = require("fs");
+	fs.readdir(mediaPath, function(err, files){
+		if(err){
+			console.log(err);
+		}else{
+			res.render('index', {title: 'H5 music', music: files});
+		}
+	});
+});
+
+module.exports = router;
+
+
